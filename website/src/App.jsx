491d9242c8f54ef8ba8f5cab9c25905b30fda9ce
Tim00tiM <olimprofl@gmail.com>
2023-03-07 23:45:14 +0300

    ui init

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,13 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import { Container, Row, Col } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      
-      
+    <div className="App" >
+      <div style={{"width": "93%", "height": "100%"}}>
+      <Container className="columnbg">
+        <Row className='columnwrapper'>
+          <Col className="column">
+            Датчики влажности и температуры теплицы
+          </Col>
+          <Col className="column">
+            Датчики бороздок
+          </Col>
+          <Col className="column">
+            Общие сведения о таблице
+          </Col>
+          <Col className="column">
+            Управление таблицей
+          </Col>
+        </Row>
+      </Container>
+      </div>
+      <div className='goddiv'>
+      <Button variant="danger" className='godbtn'>God-Mode</Button>
+      </div>
     </div>
   )
 }
